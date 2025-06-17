f457f9487c733296579d0202c94f7e5e7141fea9
yuvadhesh <yuvadheshm@gmail.com>
2025-06-17 15:57:31 +0530

    Add files via upload

---
+++ deptreply.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length,breadth;
+    cin>>length;
+    cin>>breadth;
+    int ropelength=2*(length+breadth);
+    int carpetarea=length+breadth;
+    cout<<ropelength<<endl;
+    cout<<carpetarea<<endl;
+    return 0;
+}
