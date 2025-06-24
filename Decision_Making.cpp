b423c72f5c3e6a2c84ec03a7e8c2827ac3bdedb8
yuvadhesh <yuvadheshm@gmail.com>
2025-06-24 20:00:41 +0530

    Add files via upload

---
+++ Decision_Making.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cin>>a;
+if(a>0){
+    cout<<"Positive";
+}
+else if(a<0){
+    cout<<"Negative";
+}
+else{
+    cout<<"Eual to";
+}
+
+}
